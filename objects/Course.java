5bcf1bf76c8c91f30748a18bcdbdffe0a369a14d
Kenneth Jusino <kennethj@bu.edu>
2018-11-17 11:26:58 -0500

    add course object

--- objects/Course.java
+++ objects/Course.java
@@ -1,3 +1,6 @@
+import java.util.Map;
+import java.util.HashMap;
+
 
 class Course implements Gradeable{
 	
@@ -5,11 +8,15 @@ class Course implements Gradeable{
         private CourseName courseName;
         List<Student> studentList;
         List<Assignment> assignmentList;
-		HashMap<Student, Double> finalScoreList;  
-		 
+		Map<Student, Double> finalScoreList;  
+		
 		public boolean addStudent(File file) {}
-		public boolean addStudent(Student s) {}
-		public boolean deleteStudent() {}
+		public boolean addStudent(Student s) {
+			studentList.add(s);
+		}
+		public boolean deleteStudent(Student s) {
+			studentList.remove(s);
+		}
 		 
 		public boolean addAssignment(Assignment assignment) {
 			assignmentList.add(assignment);
@@ -20,16 +27,37 @@ class Course implements Gradeable{
 		 
 		public boolean weightEqualsOne(){} 
 		public double calculateFinalScore() {
-			
+			Map finalScoreList = new Map<>();
+			for(Student student : studentList) {
+				double finalGrade = 0.0;
+				for(Assignment assignment:  assignmentList) {
+					double currentStudentRawScore = student.getAssignmentScore(assignment);
+					double currentStudentCurvedScore = currentStudentRawScore + Score.getCurvedScore();
+					if(currentStudentCurvedScore > Score.getMaxScore()) {
+						currentStudentCurvedScore = Score.getMaxScore();
+					}
+					finalGrade += (currentStudentCurvedScore * assignment.getWeight());
+				}
+				
+				this.finalScoreList.put(student, finalGrade);
+			}
 			
 		}
-		public boolean curve(Assignment assignment , int value) {}
-		public boolean changeWeight(Assignment assignment, Double newWeight) {}
+		public boolean curve(Assignment assignment , double value) {
+			
+			for(Map.entry<Student, Score> studentScores : assignment.getscoreList) {
+				studentScores.getValue().setCurvedScore(value);
+			}
+		}
+		public boolean changeWeight(Assignment assignment, double newWeight) {}
 		public boolean changeWeight(String type, Double newWeight) {}
 		public double getMax() {}
 		public double getMin() {}
-		public double calculateAverage() {}
+		public double calculateAverage() {
+			for(Map.entry<Student, Score> studentFinalScores : finalScoreList
+		}
 		public double calculateMedian() {}
 		public void printStatistics(){}
 }
+
 
