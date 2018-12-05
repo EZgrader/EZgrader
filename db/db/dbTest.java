d7ebbe3eaecad77168aa48f82fbb00a5b93a882d
Xiangyu Zhang <xzhang0531@gmail.com>
2018-12-05 12:01:35 -0800

    add db

---
+++ db/db/dbTest.java
@@ -0,0 +1,32 @@
+package db;
+
+import java.sql.SQLException;
+
+import objects.Assignment;
+import objects.Course;
+import objects.CourseName;
+import objects.Score;
+
+public class dbTest {
+	public static void main(String[] args) throws SQLException {
+		Database db = new Database();
+		db.connect("root", "sss5533");
+		db.dropEntireDb();
+		db.insertFakeData();
+		db.updateDB();
+		
+		CourseName cn = new CourseName("123", "fsdf", "sd", "evv", "2019", "wf");
+		Course a = new Course(cn);
+		db.AddCourse(a);
+		Assignment as = new Assignment("name", "cate", 0.2, 100.0, 2.0);
+		db.AddAssignment(as, 1);
+		Score s = new Score(8.0, "");
+		db.AddScore(s, "U77094012", 1, "name");
+		
+
+		System.out.println(db.studentList.size());
+		System.out.println(db.courseList.size());
+		System.out.println(db.courseList.get(0).assignmentList.size());
+		System.out.println(db.courseList.get(0).assignmentList.get(0).getScoreList().size());
+	}
+}
